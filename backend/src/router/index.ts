12a09e8dac6023a01ea6ad6c88f56a2acdf18d3d
faris <fariseeva.a.d@yandex.ru>
2025-08-21 16:54:39 +0300

    Auth-sign-up-backend

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -2,11 +2,13 @@ import { trpc } from '../lib/trpc';
 import { createPostTrpcRoute } from './createPost';
 import { getPostTrpcRoute } from './getPost';
 import { getPostsTrpcRoute } from './getPosts';
+import { signUpTrpcRoute } from './signUp';
 
 export const trpcRouter = trpc.router({
   createPost: createPostTrpcRoute,
   getPost: getPostTrpcRoute,
   getPosts: getPostsTrpcRoute,
+  signUp: signUpTrpcRoute,
 });
 
 export type TrpcRouter = typeof trpcRouter;
