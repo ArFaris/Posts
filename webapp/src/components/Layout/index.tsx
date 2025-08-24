b466f2d45d1b144fc702dd73582c15d43e2b1fc5
faris <fariseeva.a.d@yandex.ru>
2025-08-24 13:32:40 +0300

    Images bundle

--- webapp/src/components/Layout/index.tsx
+++ webapp/src/components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import { Link, Outlet } from 'react-router-dom';
+import Logo from '../../assets/images/logo.svg'
 import { useMe } from '../../lib/ctx';
 import {
   getAllPostsRoute,
@@ -16,7 +17,7 @@ export const Layout = () => {
   return (
     <div className={css.layout}>
       <div className={css.navigation}>
-        <div className={css.logo}>PostNick</div>
+        <div className={css.logo}><Logo /></div>
         <ul className={css.menu}>
           <li className={css.item}>
             <Link className={css.link} to={getAllPostsRoute()}>
