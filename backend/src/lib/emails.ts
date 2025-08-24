4f71e9fd1316e844056c3918eb526f05be238968
faris <fariseeva.a.d@yandex.ru>
2025-08-24 19:03:57 +0300

    Email handlebars

--- backend/src/lib/emails.ts
+++ backend/src/lib/emails.ts
@@ -2,75 +2,78 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import { type Post, type User } from '@prisma/client';
 import fg from 'fast-glob';
+import Handlebars from 'handlebars'
 import _ from 'lodash';
 import { env } from './env';
 
-const getHtmlTemplates = _.memoize(async () => {
-  const htmlPathsPattern = path.resolve(__dirname, '../emails/dist/**/*.html');
-  const htmlPaths = fg.sync(htmlPathsPattern);
-  const htmlTemplates: Record<string, string> = {};
-  for (const htmlPath of htmlPaths) {
-    const templateName = path.basename(htmlPath, '.html');
-    htmlTemplates[templateName] = await fs.readFile(htmlPath, 'utf8');
-  }
-  return htmlTemplates;
+const getHbrTemplates = _.memoize(async () => {
+    const htmlPathsPattern = path.resolve(__dirname, '../emails/dist/**/*.html');
+    const htmlPaths = fg.sync(htmlPathsPattern);
+    const hbrTemplates: Record<string, HandlebarsTemplateDelegate> = {}
+    for (const htmlPath of htmlPaths) {
+        const templateName = path.basename(htmlPath, '.html')
+        const htmlTemplate = await fs.readFile(htmlPath, 'utf8')
+        hbrTemplates[templateName] = Handlebars.compile(htmlTemplate)
+    }
+    return hbrTemplates
 });
 
-const getHtmlTemplate = async (templateName: string) => {
-  const htmlTemplates = await getHtmlTemplates();
-  return htmlTemplates[templateName];
+const getEmailHtml = async (templateName: string, templateVariables: Record<string, string> = {}) => {
+    const hbrTemplates = await getHbrTemplates()
+    const hbrTemplate = hbrTemplates[templateName]
+    const html = hbrTemplate(templateVariables)
+    return html
 };
 
 const sendEmail = async ({
-  to,
-  subject,
-  templateName,
-  templateVariables = {},
+    to,
+    subject,
+    templateName,
+    templateVariables = {},
 }: {
-  to: string;
-  subject: string;
-  templateName: string;
-  templateVariables?: Record<string, any>;
+    to: string;
+    subject: string;
+    templateName: string;
+    templateVariables?: Record<string, any>;
 }) => {
-  try {
-    const htmlTemplate = await getHtmlTemplate(templateName);
-    const fullTemplateVaraibles = {
-      ...templateVariables,
-      homeUrl: env.WEBAPP_URL,
-    };
-    console.info('sendEmail', {
-      to,
-      subject,
-      templateName,
-      fullTemplateVaraibles,
-      htmlTemplate,
-    });
-    return { ok: true };
-  } catch (error) {
-    console.error(error);
-    return { ok: false };
-  }
+    try {
+        const fullTemplateVaraibles = {
+            ...templateVariables,
+            homeUrl: env.WEBAPP_URL,
+        };
+        const html = await getEmailHtml(templateName, fullTemplateVaraibles)
+        console.info('sendEmail', {
+            to,
+            subject,
+            templateName,
+            html,
+        });
+        return { ok: true };
+    } catch (error) {
+        console.error(error);
+        return { ok: false };
+    }
 };
 
 export const sendWelcomeEmail = async ({ user }: { user: Pick<User, 'nick' | 'email'> }) => {
-  return await sendEmail({
-    to: user.email,
-    subject: 'Thanks For Registration!',
-    templateName: 'welcome',
-    templateVariables: {
-      userNick: user.nick,
-      addPostUrl: `${env.WEBAPP_URL}/posts/new`,
-    },
-  });
+    return await sendEmail({
+        to: user.email,
+        subject: 'Thanks For Registration!',
+        templateName: 'welcome',
+        templateVariables: {
+            userNick: user.nick,
+            addPostUrl: `${env.WEBAPP_URL}/posts/new`,
+        },
+    });
 };
 
 export const sendPostBlockedEmail = async ({ user, post }: { user: Pick<User, 'email'>; post: Pick<Post, 'nick'> }) => {
-  return await sendEmail({
-    to: user.email,
-    subject: 'Your Post Blocked!',
-    templateName: 'postBlocked',
-    templateVariables: {
-      postNick: post.nick,
-    },
-  });
+    return await sendEmail({
+        to: user.email,
+        subject: 'Your Post Blocked!',
+        templateName: 'postBlocked',
+        templateVariables: {
+            postNick: post.nick,
+        },
+    });
 };
